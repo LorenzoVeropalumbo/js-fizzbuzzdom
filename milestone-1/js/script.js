14d0efce6e4888adf4362467e023f3a0a39c3826
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-06-07 14:29:51 +0200

    fix problem

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -4,25 +4,25 @@ for(let i = 1; i <= 100; i++) {
   
   if(i % 3 == 0 && i % 5 == 0){
 
-    const newLi = `<h2 class="mia-classe">FizzBuzz</h2>`;
+    const newLi = `<h2>FizzBuzz</h2>`;
     numberList.innerHTML += newLi;
     console.log("FizzBuzz");
 
   } else if (i % 3 == 0) {
 
-    const newLi = `<h2 class="mia-classe">Fizz</h2>`;
+    const newLi = `<h2>Fizz</h2>`;
     numberList.innerHTML += newLi;
     console.log("Fizz");
 
   } else if (i % 5 == 0) {
     
-    const newLi = `<h2 class="mia-classe">Buzz</h2>`;
+    const newLi = `<h2>Buzz</h2>`;
     numberList.innerHTML += newLi;
     console.log("Buzz");
 
   } else {
 
-    const newLi = `<h2 class="mia-classe">${i}</h2>`;
+    const newLi = `<h2>${i}</h2>`;
     numberList.innerHTML += newLi;
     console.log(i);
   }
