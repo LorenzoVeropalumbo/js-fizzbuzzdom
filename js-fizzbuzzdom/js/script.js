feb995c851841bd6a8ce72419b3ea3b45d40fd0e
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-06-07 14:29:24 +0200

    creation template

--- js-fizzbuzzdom/js/script.js
+++ js-fizzbuzzdom/js/script.js
@@ -2,7 +2,7 @@ const numberList = document.querySelector('.list');
 
 for(let i = 1; i <= 100; i++) {
   
-  const newLi = `<h2 class="mia-classe">${i}</h2>`;
+  const newLi = `<h2>${i}</h2>`;
   numberList.innerHTML += newLi;
   console.log(i);
 }
--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -1,30 +1,28 @@
 const numberList = document.querySelector('.list');
-
+let FizzBuzz;
 for(let i = 1; i <= 100; i++) {
   
   if(i % 3 == 0 && i % 5 == 0){
 
-    const newLi = `<h2 class="mia-classe">FizzBuzz</h2>`;
+    const newLi = `<li>FizzBuzz</li>`;
     numberList.innerHTML += newLi;
-    console.log("FizzBuzz");
+    
 
   } else if (i % 3 == 0) {
 
-    const newLi = `<h2 class="mia-classe">Fizz</h2>`;
+    const newLi = `<li>Fizz</li>`;
     numberList.innerHTML += newLi;
-    console.log("Fizz");
 
   } else if (i % 5 == 0) {
     
-    const newLi = `<h2 class="mia-classe">Buzz</h2>`;
+    const newLi = `<li>Buzz</li>`;
     numberList.innerHTML += newLi;
-    console.log("Buzz");
 
   } else {
 
-    const newLi = `<h2 class="mia-classe">${i}</h2>`;
+    const newLi = `<li>${i}</li>`;
     numberList.innerHTML += newLi;
-    console.log(i);
+
   }
   
 }
